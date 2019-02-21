294e8bc39e65cd841680a70b6763dba633d17bc6
Cigery <150526401@qq.com>
2019-02-21 22:37:42 +0800

    修复重复实例化sendmail_limit_model类问题

--- protected/model/sendmail_limit_model.php
+++ protected/model/sendmail_limit_model.php
@@ -17,8 +17,7 @@ class sendmail_limit_model extends Model
      */
     public function check_counts($type, $user_id = 0, $ip = '')
     {
-        $limit_model = new sendmail_limit_model();
-        if($count = $limit_model->find(array('type' => $type, 'user_id' => $user_id, 'ip' => $ip, 'dateline' => strtotime(date('Ymd'))), null, 'count'))
+        if($count = $this->find(array('type' => $type, 'user_id' => $user_id, 'ip' => $ip, 'dateline' => strtotime(date('Ymd'))), null, 'count'))
         {
             if($count['count'] >= 5) return FALSE;
         }
